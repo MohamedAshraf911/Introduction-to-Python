3bf5796962bd3ce09d30a70608375c9846b64a37
Mohamed Ashraf <96884905+MohamedAshraf911@users.noreply.github.com>
2025-01-05 00:39:03 +0200

    First Python Project

---
+++ Python Tutorial/Module Assessments/Module Assessment 1/first.py
@@ -0,0 +1,4 @@
+info = """My name is Mohamed Ashraf, I'm 24 years old,
+in this course I want to learn using python in professional way, use it in automation testing, know 
+intro about ML, AI, Data analysis"""
+print(info)
