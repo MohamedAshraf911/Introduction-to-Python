38f4e031ab31e6187ab21b7ee4073e9e6fedbee5
MohamedAshraf <ashrafoabbas73@yahoo.com>
2025-01-05 01:53:29 +0200

    Age Calculator Mini Project

---
+++ Age Calculator/main.py
@@ -0,0 +1,4 @@
+start_year =input("Enter your birth year: ")
+end_year = input("Enter the year you want to calculate your age: ")
+age = int(end_year) - int(start_year)
+print(f"Your age is {age} years old")
