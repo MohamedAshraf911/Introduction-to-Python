99755ba8e8b666e44fdf5dba89c0f2754d620979
MohamedAshraf <ashrafoabbas73@yahoo.com>
2025-01-05 10:43:03 +0200

    The Fibonacci Sequence Mini Project

---
+++ Fibonacci sequence/main.py
@@ -0,0 +1,23 @@
+n = input("Enter the number for fibonacci sequence: ")
+a = 0
+b = 1
+
+while n.isdigit() != True:
+    print("Please enter a valid number")
+    n = input("Enter the number for fibonacci sequence: ")
+
+while int(n) <= 0:
+    print("Please enter a positive number")
+    n = input("Enter the number for fibonacci sequence: ") 
+
+
+if int(n) == 1:
+    print(a)
+else:
+    print(a)
+    print(b)
+    for i in range(2, int(n)):
+        c = a + b
+        a = b
+        b = c
+        print(c)
