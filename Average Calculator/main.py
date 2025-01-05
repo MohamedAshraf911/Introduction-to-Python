aac751db0d08cac72f7302a748eaaad769059a4e
MohamedAshraf <ashrafoabbas73@yahoo.com>
2025-01-05 13:02:46 +0200

    Optimized Average Calculator

--- Average Calculator/main.py
+++ Average Calculator/main.py
@@ -1,5 +1,13 @@
 arr = input("Enter the numbers separated by space: ").split()
 
+if not arr:
+    print("No numbers provided")
+    exit()
+
+while not all(num.isdigit() for num in arr):
+    print("Please enter valid numbers")
+    arr = input("Enter the numbers separated by space: ").split()
+
 def calculate_average(numbers):
     return sum(numbers) / len(numbers)
 
